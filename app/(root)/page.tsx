f2a7a1914b01ae3eeea57a6db572e91ad4ffeaf9
RohmadRifai <rohmad04rifai@gmail.com>
2025-02-03 16:55:27 +0700

    feat: implement authentication with NextAuth and GitHub provider, create NavBar component, and set up Tailwind CSS configuration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+      </section>
+    </>
+  );
+}
