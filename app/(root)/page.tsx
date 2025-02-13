273ad15a2b463984635968b3015441c4bd1bf0bd
RohmadRifai <rohmad04rifai@gmail.com>
2025-02-13 15:18:49 +0700

    feat: integrate Sanity for content management, add author and startup schemas, implement live content fetching, and enhance homepage with search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
+import { title } from "process";
 import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -6,6 +11,8 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -20,6 +27,24 @@ export default async function Home({
         </p>
         <SearchForm query={query} />
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
     </>
   );
 }
