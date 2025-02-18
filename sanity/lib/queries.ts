1e67f6a2b8fb33a43bf2128de4ee4e49a33c121d
RohmadRifai <rohmad04rifai@gmail.com>
2025-02-18 11:30:55 +0700

    feat: enhance startup page layout with author details, image, and pitch information

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,20 +12,21 @@ export const STARTUPS_QUERY =
   views,
   description,
   category,
-  image
+  image,
 }`);
 
 export const STARTUP_BY_ID_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current)]{
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id,
   title,
   slug,
   _createdAt,
   author -> {
-    _id, name, image, bio
+    _id, name, username, image, bio
   },
   views,
   description,
   category,
-  image
+  image,
+  pitch,
 }`);
